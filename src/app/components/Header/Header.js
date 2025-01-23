f8bc7fea692bba7caf1ede72f0ec8e8ba5ed5c2d
User <diduhs0@gmail.com>
2025-01-23 20:01:06 +0200

    Create Header, Hero, List

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,73 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../../public/images/logo.svg";
+import styles from "./Header.module.scss";
+import { useState, useEffect } from "react";
+
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // useEffect(() => {
+  //   const body = document.body;
+  //   if (menuOpen) {
+  //     body.classList.add('menu-open');
+  //     console.log(body.classList);
+      
+  //   } else {
+  //     body.classList.remove('menu-open');
+  //   }
+  
+  //   return () => {
+  //     body.classList.remove('menu-open');
+  //   };
+  // }, [menuOpen]);
+  
+
+  return (
+    <div className={styles.header__container}>
+      <nav className={styles.header__nav}>
+        <Image src={logo} alt="logo" className={styles.header__logo} />
+        <ul
+          className={`${styles.header__items} ${menuOpen ? styles.active : ""}`}
+        >
+          <li className={styles.header__item}>
+            <Link className={styles.header__link} href="/">
+              Портфоліо
+            </Link>
+          </li>
+          <li className={styles.header__item}>
+            <Link className={styles.header__link} href="/">
+              Сервіси
+            </Link>
+          </li>
+          <li className={styles.header__item}>
+            <Link className={styles.header__link} href="/">
+              Блог
+            </Link>
+          </li>
+          <li className={styles.header__item}>
+            <Link className={styles.header__link} href="/">
+              Контакти
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.header__btns}>
+          <button className={styles.header__btn_lang}>EN</button>
+          <Link href="/" className={styles.header__btn_yellow}>
+            Задати запитання
+          </Link>
+          <button
+            className={styles.header__ham_menu}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </ul>
+      </nav>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,98 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import Header from "./components/Header/Header";
+import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <section className={styles.hero}>
+        <h1 className={styles.hero__title}>Ми — Abra<br />
+        Креативна веб-агенція</h1>
+        <p className={styles.hero__subtitle}>Ми команда творчих дизайнерів та розробників, які перетворюють ідеї на захоплюючі візуальні рішення. Маємо експертизу у розробці веб-сайтів з високою оптимізацією для пошукових систем (SEO).</p>
+        <p className={styles.hero__subtitle}>Нашою метою є надання високоякісного та інноваційного дизайну, який підвищує видимість вашого бізнесу в онлайн-середовищі.</p>
+        <div className={styles.hero__btns}>
+          <Link href='/' className={styles.hero__btn_white}>Наші проєкти <FontAwesomeIcon className={styles.hero__btn_icon} icon={faArrowRight} /></Link>
+          <Link href='/' className={styles.hero__btn_yellow}>Безкоштовна консультація<FontAwesomeIcon className={styles.hero__btn_icon} icon={faArrowRight} /></Link>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+      <section className={styles.list}>
+        <h2 className={styles.list__title}>Наша агенція пропонує повний спектр послуг, оптимізованих для максимальної ефективності та задоволення потреб клієнтів.</h2>
+        <p className={styles.list__subtitle}>Зосередьтеся на тому, що ви робите найкраще, а решту завдань з<br />оцифрування бізнесу довірте <Link className={styles.list__subtitle_link} href='/'>Abra Agency</Link> !</p>
+        <ul className={styles.list__items}>
+          <li>
+            <h3 className={styles.list__col_title}>Веб сайти</h3>
+            <ul className={styles.list__col}>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Аналіз/Аудит</Link>
+              </li>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                UI/UX</Link>
+              </li>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Редизайн</Link>
+              </li>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Корпоративний веб-сайт</Link>
+              </li>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Інтернет-магазин</Link>
+              </li>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Лендінг сторінка</Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3 className={styles.list__col_title}>Брендинг</h3>
+            <ul className={styles.list__col}>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Лого</Link>
+              </li>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Брендбук</Link>
+              </li>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Копірайтинг</Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3 className={styles.list__col_title}>Техпідтримка</h3>
+            <ul className={styles.list__col}>
+              <li>
+                <Link href='/' className={styles.list__col_item}>
+                <span className={styles.list__decor}></span>
+                Підтримка сайту</Link>
+              </li>
+              
+            </ul>
+          </li>
+        </ul>
+        <Link href='/' className={styles.list__btn}>Переглянути всі послуги <FontAwesomeIcon className={styles.hero__btn_icon} icon={faArrowRight} /></Link>
+
+      </section>
     </div>
   );
 }
